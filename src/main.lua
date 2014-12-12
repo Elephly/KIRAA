1e54d6c40b61230f2827810d0d82f4e93c8f9c22
Nick <elephly@live.com>
2014-12-12 14:13:45 -0500

    Now using love-maker
    Presenting... LOVE-MAKER!!

--- src/main.lua
+++ src/main.lua
@@ -8,19 +8,27 @@ local framesPerSecond = 60
 local player
 
 function love.load()
+  love.graphics.setBackgroundColor(255, 255, 255)
   player = newPlayer(love.graphics.getWidth() / 2, love.graphics.getHeight() * 3 / 4)
 end
 
 function love.update(deltaTime)
+  if love.keyboard.isDown("escape") then
+    love.event.quit()
+  end
   dTotalPrecise = dTotalPrecise + deltaTime
   if dTotalPrecise >= 1 then
     dTotalPrecise = dTotalPrecise - 1
     dTotalSeconds = dTotalSeconds + 1
   end
   framesPerSecond = 1 / deltaTime
+  
+  player:update(deltaTime)
 end
 
 function love.draw()
+  love.graphics.setColor(0, 0, 0)
   love.graphics.print("Uptime: " .. dTotalSeconds .. " seconds\nFPS: " .. string.format("%d", framesPerSecond), 0, 0)
+  love.graphics.setColor(255, 255, 255)
   player:draw()
 end
--- src/player.lua
+++ src/player.lua
@@ -9,8 +9,14 @@ local player = {
   }
 }
 
+function player:update(dt)
+  handleInput(self, dt)
+end
+
 function player:draw()
+  love.graphics.setColor(0, 0, 0)
   love.graphics.rectangle("fill", self.x - self.origin.x, self.y - self.origin.y, self.width, self.height)
+  love.graphics.setColor(255, 255, 255)
 end
 
 function newPlayer(xPos, yPos)
@@ -23,7 +29,17 @@ function newPlayer(xPos, yPos)
       x = (player.width / 2),
       y = (player.height / 2)
     },
+    update = player.update,
     draw = player.draw
   }
   return p
+end
+
+function handleInput(p, dt)
+  if love.keyboard.isDown('a') then
+    p.x = p.x - 100 * dt
+  end
+  if love.keyboard.isDown('d') then
+    p.x = p.x + 100 * dt
+  end
 end
